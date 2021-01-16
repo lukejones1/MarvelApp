6072e5eda87497fa6355b158ba0888238daba672
Luke Jones <luke_09@hotmail.co.uk>
2021-01-16 14:40:17 +0000

    Create custom result types for client and loader

--- MarvelFeedTests/CharacterLoaderTests.swift
+++ MarvelFeedTests/CharacterLoaderTests.swift
@@ -1,7 +1,8 @@
 import Foundation
 
 protocol HTTPClient {
-    func get(_ url: URL, completion: @escaping  (HTTPURLResponse?, Error?) -> Void)
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+    func get(_ url: URL, completion: @escaping  (Result) -> Void)
 }
 
 class CharacterLoader {
@@ -14,20 +15,23 @@ class CharacterLoader {
         case invalidData
     }
     
+    public typealias Result = Swift.Result<[Character], Error>
+    
     init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
     
-    func load(completion: @escaping (Error) -> Void) {
-        client.get(url) { response, error in
-            guard error == nil else {
-                completion(.connectivity)
-                return
-            }
-            guard response?.statusCode == 200 else {
-                completion(.invalidData)
-                return
+    func load(completion: @escaping (Result) -> Void) {
+        client.get(url) { result in
+            switch result {
+            case let .success(_, response):
+                guard response.statusCode == 200 else {
+                    completion(.failure(.invalidData))
+                    return
+                }
+            case .failure:
+                completion(.failure(.connectivity))
             }
         }
     }
@@ -66,7 +70,7 @@ class CharacterLoaderTests: XCTestCase {
         let clientError = NSError(domain: "client", code: 0)
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWith: .connectivity, when: {
+        expect(sut, toCompleteWith: .failure(.connectivity), when: {
             client.complete(with: clientError)
         })
         
@@ -77,7 +81,7 @@ class CharacterLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
         
         sampleCodes.enumerated().forEach { index, code in
-            expect(sut, toCompleteWith: .invalidData, when: {
+            expect(sut, toCompleteWith: .failure(.invalidData), when: {
                 client.complete(with: code, at: index)
             })
         }
@@ -91,43 +95,43 @@ class CharacterLoaderTests: XCTestCase {
         return (sut, client)
     }
     
-    private func expect(_ sut: CharacterLoader, toCompleteWith error: CharacterLoader.Error, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+    private func expect(_ sut: CharacterLoader, toCompleteWith result: CharacterLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for load completion")
-        var receivedError: CharacterLoader.Error?
-        sut.load() { (error) in
-            receivedError = error
+        var receivedResult: CharacterLoader.Result?
+        sut.load() { result in
+            receivedResult = result
             exp.fulfill()
         }
         
         action()
         wait(for: [exp], timeout: 1.0)
         
-        XCTAssertEqual(receivedError, error, file: file, line: line)
+        XCTAssertEqual(receivedResult, result, file: file, line: line)
     }
     
     class HTTPClientSpy: HTTPClient {
-        var messages: [(url: URL, completion: (HTTPURLResponse?, Error?) -> Void)] = []
+        var messages: [(url: URL, completion: (HTTPClient.Result) -> Void)] = []
         
         var urls: [URL] {
             return messages.map { $0.url }
         }
 
-        func get(_ url: URL, completion: @escaping (HTTPURLResponse?, Error?) -> Void) {
+        func get(_ url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
             messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(nil, error)
+            messages[index].completion(.failure(error))
         }
         
-        func complete(with statusCode: Int, at index: Int = 0) {
+        func complete(with statusCode: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: urls[index],
                 statusCode: statusCode,
                 httpVersion: nil,
                 headerFields: nil
-            )
-            messages[index].completion(response, nil)
+            )!
+            messages[index].completion(.success((data, response)))
         }
     }
 }
