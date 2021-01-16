3f7fe17dab747756d95ab3494393c1cbe518e151
Luke Jones <luke_09@hotmail.co.uk>
2021-01-16 10:26:31 +0000

    Create character feature

---
+++ MarvelFeed/Character Feature/Character.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public struct Character: Equatable {
+    public var id: Int
+    public var name: String?
+    public var description: String?
+    public var imageURL: URL?
+}
---
+++ MarvelFeed/Character Feature/CharacterLoader.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+protocol CharacterLoader {
+    typealias Result = Swift.Result<Character, Error>
+    func load(completion: @escaping (Result) -> Void)
+}
